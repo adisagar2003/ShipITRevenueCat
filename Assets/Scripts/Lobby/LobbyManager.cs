3502c1ad170b85f680f1b9213874ec89f600304f
Adi <adityasagarpython@gmail.com>
2025-07-16 21:50:04 -0300

    feat: Added create lobby

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -33,6 +33,7 @@ public class LobbyManager : MonoBehaviour
             foreach (Lobby lobby in availableLobbies)
             {
                 Debug.Log($"Lobby Name: {lobby.Name}, Lobby Code: {lobby.LobbyCode}");
+                await GetAvailableLobbiesAsync();
             }
         }
         catch (LobbyServiceException e)
@@ -40,5 +41,40 @@ public class LobbyManager : MonoBehaviour
             Debug.LogError($"Failed to get lobbies: {e}");
         }
     }
+    
+    public async void CreateLobbyAsync(string lobbyName = "MyLobby", int maxPlayers = 2)
+    {
+        try
+        {
+            CreateLobbyOptions options = new CreateLobbyOptions
+            {
+                IsPrivate = false,
+                Data = new Dictionary<string, DataObject>
+            {
+                { "gameMode", new DataObject(DataObject.VisibilityOptions.Public, "casual") }
+            }
+            };
+
+            Lobby createdLobby = await Lobbies.Instance.CreateLobbyAsync(lobbyName, maxPlayers, options);
+
+            Debug.Log($"Lobby created successfully!");
+            Debug.Log($"Lobby Name: {createdLobby.Name}");
+            Debug.Log($"Lobby ID: {createdLobby.Id}");
+            Debug.Log($"Lobby Code: {createdLobby.LobbyCode}");
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError($"Failed to create lobby: {e}");
+        }
+    }
+
+    [ContextMenu("Create Button on click")]
+    public void OnCreateLobbyButtonClicked()
+    {
+        _ = CreateLobbyAsync();
+    }
+
+
+
 
 }
