a146d6ed1eeb994818ee29c23e2f38290a015ccf
Adi <adityasagarpython@gmail.com>
2025-07-21 17:58:14 -0300

    bugfix: Animation sync fixed

--- Assets/PlayerAnimationHandle.cs
+++ Assets/PlayerAnimationHandle.cs
@@ -1,79 +1,41 @@
 #define multiplayer
+//#define GUIDebug // Uncomment to enable on-screen debug
+
 using UnityEngine;
 using Unity.Netcode;
 
 public class PlayerAnimationHandle : NetworkBehaviour
 {
-    private Rigidbody rb;
     private Animator animator;
-
-    [SerializeField] private float minSpeedThreshold = 0.2f;
-    [SerializeField, TextArea] private string debugString;
+    private Rigidbody rb;
     private bool previousIsRunning = false;
 
-    private NetworkVariable<bool> isRunningNetVar = new NetworkVariable<bool>(
-        false,
-        NetworkVariableReadPermission.Everyone,
-        NetworkVariableWritePermission.Server
-    );
+    [SerializeField] private float minSpeedThreshold = 0.2f;
 
     public override void OnNetworkSpawn()
     {
-        rb = GetComponentInParent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         animator = GetComponentInChildren<Animator>();
-
-        if (animator == null)
-            Debug.LogWarning("Animator not found on PlayerAnimationHandle!");
-
-        if (rb == null)
-            Debug.LogWarning("Rigidbody not found on PlayerAnimationHandle!");
-
-        isRunningNetVar.OnValueChanged += (oldVal, newVal) =>
-        {
-            if (animator != null)
-            {
-                animator.SetBool("isRunning", newVal);
-                debugString += $"[Sync] isRunning changed to: {newVal}\n";
-            }
-        };
     }
+
     private void Update()
     {
-#if multiplayer
         if (!IsOwner) return;
-#endif
-        HandleSprintAnimation();
-    }
 
-    private void HandleSprintAnimation()
-    {
-        if (rb == null) return;
+        bool isRunning = rb.velocity.magnitude > minSpeedThreshold;
 
-        bool localIsRunning = rb.velocity.magnitude > minSpeedThreshold;
-        if (localIsRunning != previousIsRunning)
+        if (isRunning != previousIsRunning)
         {
-            SubmitIsRunningServerRpc();
-            previousIsRunning = localIsRunning;
+            animator.SetBool("isRunning", isRunning);
+            SubmitIsRunningServerRpc(isRunning);
+            previousIsRunning = isRunning;
         }
     }
 
     [ServerRpc]
-    private void SubmitIsRunningServerRpc()
+    private void SubmitIsRunningServerRpc(bool isRunning)
     {
-        if (rb == null) return;
-
-        // Server decides if player is running based on authoritative velocity
-        bool serverIsRunning = rb.velocity.magnitude > minSpeedThreshold;
-        isRunningNetVar.Value = serverIsRunning;
+        if (animator == null) return;
+        animator.SetBool("isRunning", isRunning);
     }
-
-#if GUIDebug
-    private void OnGUI()
-    {
-#if multiplayer
-        if (!IsOwner) return;
-#endif
-        GUI.Label(new Rect(20, 10, 600, 200), debugString);
-    }
-#endif
 }
